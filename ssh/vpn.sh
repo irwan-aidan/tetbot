6ec23140fc91f5951dad291854001bab53c9bfae
mohd irwan mohi <91520148+irwan-aidan@users.noreply.github.com>
2022-06-08 10:11:55 +0800

    Update vpn.sh

--- ssh/vpn.sh
+++ ssh/vpn.sh
@@ -93,9 +93,9 @@ apt-get install openvpn
 
 # Install the latest version of easy-rsa from source, if not already installed.
         easy_rsa_url='https://github.com/OpenVPN/easy-rsa/releases/download/v3.1.0/EasyRSA-3.1.0.tgz'
-        mkdir -p /etc/openvpn/server/easy-rsa/
-        { wget -qO- "$easy_rsa_url" 2>/dev/null || curl -sL "$easy_rsa_url" ; } | tar xz -C /etc/openvpn/server/easy-rsa/ --strip-components 1
-        chown -R root:root /etc/openvpn/server/easy-rsa/
+        mkdir -p /etc/openvpn/easy-rsa/
+        { wget -qO- "$easy_rsa_url" 2>/dev/null || curl -sL "$easy_rsa_url" ; } | tar xz -C /etc/openvpn/easy-rsa/ --strip-components 1
+        chown -R root:root /etc/openvpn/easy-rsa/
         cd /etc/openvpn/easy-rsa/
 
 cat > /etc/openvpn/easy-rsa/vars << EOF
